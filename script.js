449b20e055e79626b5a2f3eb83018956d9132966
ma9kli <pain17.si19@gmail.com>
2025-02-02 15:24:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// وظيفة إضافة مراجعة للمستخدمين
+document.querySelectorAll('.add-review').forEach(button => {
+    button.addEventListener('click', function() {
+        alert("أضف مراجعتك هنا.");
+    });
+});
